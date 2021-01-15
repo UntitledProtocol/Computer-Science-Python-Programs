8d71041d9dfaa17b486564dfbe15365759f2e4ce
Rongzhen Li <untitledprotocol@gmail.com>
2021-01-15 14:11:22 +0000

    finished task 2

---
+++ task2_student_data.py
@@ -0,0 +1,6 @@
+studentdata = ["bob", "something", "-2", "FALSE"]
+
+if studentdata[3] == "FALSE": # I'm assuming the fourth item is the relevent value
+    print("sent")
+else:
+    print("not sent")
